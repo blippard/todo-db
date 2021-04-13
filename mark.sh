9fd29be157782544c3a6692a80c8e70febc65823
blippard <blippard@gmail.com>
2021-04-13 10:24:45 +0200

    Add: added mark function to change boolean value of task

--- mark.sh
+++ mark.sh
@@ -8,6 +8,18 @@
 #    mark.sh unmark-todo 32
 #
 
+DATABASE="tododb"
+export PGPASSWORD=${PSQL_PASSWORD}
+
+mark() {
+  todo_id=$1
+  psql -h ${PSQL_HOST} -U ${PSQL_USER_NAME} $DATABASE <<EOF
+  UPDATE "todo" SET "done" = TRUE WHERE "todo_id" = $todo_id
+EOF
+  todo_task=`psql -X -A -d $DATABASE -U ${PSQL_USER_NAME} -h localhost -p 5432 -t -c "SELECT task FROM "todo" WHERE "todo_id"=$todo_id"`
+echo "TODO: $todo_task - Marked as done"
+}
+
 main() {
     if [[ "$#" -lt 2 ]]
     then
