c60715c92dee157cfa86194b60b8e9cb0474ffbf
blippard <blippard@gmail.com>
2021-04-15 12:08:12 +0200

    Add: command routing and help text

--- todo.sh
+++ todo.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
-. list.sh
 
-if [[ "$1" == "list-users" ]]
+help() {
+  echo "Please use the following commands in this format:
+            ./todo.sh [commands] [options] [...arguments]
+
+        Commands use the following structure:
+            list-users [options] [...arguments]
+              [options]
+                list-users,                 lists all users
+                list-todos,                 lists all todos
+                list-user-todos [username], list all users and todos,
+                                            or specific user's todos
+            add [options] [...arguments]
+              [options]
+                add-user [username],        adds user
+                add-todo [username] [todo], adds todo to user
+            mark-todos [options] [...arguments]
+              [options]
+                mark [todo id],             marks todo as done
+                unmark [todo id],           marks todo as in progress
+            delete [options] [...arguments]
+              [options]
+                delete-todo [todo id],      deletes specific todo
+                delete-done,                deletes all todos with done status
+  "
+}
+
+main() {
+  if [[ "$1" == "list-users" ]]; then
+    ./list.sh "$1"
+  elif [[ "$1" == "add" ]]; then
+    ./add.sh "$1" "$2"
+  elif [[ "$1" == "mark-todos" ]]; then
+    ./mark.sh "$1" "$2"
+  elif [[ "$1" == "delete" ]]; then
+    ./delete.sh "$1" "$2"
+  elif [[ "$1" == "help" ]]; then
+    help
+  else
+    echo "Please type help for additional info.
+
+              ./todo.sh help"
+  fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
-    list_users
+    main "$@"
 fi
