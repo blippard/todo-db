f4c1141ef14656ab670f964a0643935fbfce9837
blippard <blippard@gmail.com>
2021-04-15 12:07:45 +0200

    Edit: added error handling

--- list.sh
+++ list.sh
@@ -15,23 +15,27 @@ DATABASE="tododb"
 export PGPASSWORD=${PSQL_PASSWORD}
 
 list_users() {
-    psql -h ${PSQL_HOST} -U ${PSQL_USER_NAME} $DATABASE <<EOF
+    psql -h "${PSQL_HOST}" -U "${PSQL_USER_NAME}" $DATABASE <<EOF
 SELECT * FROM "user"
 EOF
 }
 
 list_todos() {
-    psql -h ${PSQL_HOST} -U ${PSQL_USER_NAME} $DATABASE <<EOF
+    psql -h "${PSQL_HOST}" -U "${PSQL_USER_NAME}" $DATABASE <<EOF
     SELECT todo_id AS id, task, CASE WHEN done = TRUE THEN 'done' ELSE 'in progress' END AS status FROM "todo"
 EOF
 }
 
 list_user_todos() {
   user=$1
-    psql -h ${PSQL_HOST} -U ${PSQL_USER_NAME} $DATABASE <<EOF
+    psql -h "${PSQL_HOST}" -U "${PSQL_USER_NAME}" $DATABASE -v ON_ERROR_STOP=1 2> /dev/null <<EOF
     SELECT name, task, todo_id AS id, CASE WHEN done = TRUE THEN 'done' ELSE 'in progress' END AS status FROM "todo" JOIN "user" u on u.user_id = todo.user_id
     WHERE name ILIKE '$user%'
 EOF
+
+  if [[ "$?" -eq 3 ]]; then
+    echo "No user named $user found. Please add user first with ./add.sh add-user [username]"
+  fi
 }
 
 main() {
@@ -40,8 +44,8 @@ main() {
       echo "Missing arguments:
             list.sh [arguments] [username]
             - list-todos
-            - list-user
-            - list-user-todo [username]"
+            - list-users
+            - list-user-todos [username]"
     elif [[ "$1" == "list-users" ]]
     then
         list_users
